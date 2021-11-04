49b0ec392f393a8313df52c77f8368ec945ad6b6
tianyu139 <liutianyu100@gmail.com>
2021-11-03 23:48:59 -0700

    Refactor to use .env for firebase env vars

--- frontend/club-recruiter-ui/src/api/firebase.js
+++ frontend/club-recruiter-ui/src/api/firebase.js
@@ -2,15 +2,14 @@ import { initializeApp } from 'firebase/app';
 import {
   getAuth, signInWithPopup, GoogleAuthProvider,
 } from 'firebase/auth';
-import env from '../config.json';
 
 const firebaseConfig = {
-  apiKey: env.apiKey,
-  authDomain: env.authDomain,
-  projectId: env.projectId,
-  storageBucket: env.storageBucket,
-  messagingSenderId: env.messagingSenderId,
-  appId: env.appId,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
