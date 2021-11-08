2297c14a9364816bc28bfd2780158e3b625286d6
Andy Cai <andycai20000212@gmail.com>
2021-11-08 13:12:11 -0800

    Added Firebase Admin service config

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,9 +1,26 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions'
+import * as express from 'express'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import * as admin from 'firebase-admin'
+import * as serviceAccount from '../config/serviceAccountKey.json'
+
+const params = {
+  type: serviceAccount.type,
+  projectId: serviceAccount.project_id,
+  privateKeyId: serviceAccount.private_key_id,
+  privateKey: serviceAccount.private_key,
+  clientEmail: serviceAccount.client_email,
+  clientId: serviceAccount.client_id,
+  authUri: serviceAccount.auth_uri,
+  tokenUri: serviceAccount.token_uri,
+  authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
+  clientC509CertUrl: serviceAccount.client_x509_cert_url
+}
+
+admin.initializeApp({
+  credential: admin.credential.cert(params),
+});
+
+const app = express()
+app.get('/', (req, res) => res.status(200).send('Hey there!'))
+exports.app = functions.https.onRequest(app)
