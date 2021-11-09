e0f8c1bb1e47cf5ffc2bb66c07bdf043d50cd731
Andy Cai <andycai20000212@gmail.com>
2021-11-09 00:36:24 -0800

    Added authentication middleware

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,26 +1,31 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 const express = require('express')
-const cookieParser = require('cookie-parser')
+var app = express();
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const { validateFirebaseIdToken } = require('./auth')
 
-admin.initializeApp();
-
-// candidate router
+// routers
 const candidateRouter = require('./routes/candidates')
-
 const eventRouter = require('./routes/events')
 
-var app = express();
+// initialize firestore app
+admin.initializeApp();
 
+app.use(cors({ origin: true }));
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => res.status(200).send('Hey there!'))
 
-// set up candidate Router with the prefix /candidates
+// set up candidate Router with the prefix /candidate
 app.use("/candidate", candidateRouter);
-// app.use("/events", eventRouter);
+// auth protected version below, uncomment when testing is finished
+// app.use("/event", validateFirebaseIdToken, eventRouter);
+app.use("/event", eventRouter);
 
 
 exports.app = functions.https.onRequest(app)
--- backend/functions/routes/events.js
+++ backend/functions/routes/events.js
@@ -7,16 +7,19 @@ var router = express.Router();
  * @param {req} contains a ClubMember's userID
  * @returns a list of events the ClubMember is a *member* of
  */
-router.get('/list', async function (req, res, next) {
+router.get('/list', async function (req, res) {
   try {
-    var member_id = req.params.member_id;
+    var { member_id } = req.params;
 
     var db = admin.firestore();
     const eventListRes = await db.collection("events").where("member_id", "==", member_id).get();
 
+    var eventList = [];
     eventListRes.forEach(doc => {
+      eventList.push(doc.data());
       console.log(doc.id, '=>', doc.data());
     });
+    res.status(200).send(eventList);
   } catch (e) {
     res.status(404).send(`Error adding document with ID: ${docRef.id}`);
   }
