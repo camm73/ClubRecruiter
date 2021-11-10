860d093667106944b602cb3fcb61a356155efaaf
Andy Cai <andycai20000212@gmail.com>
2021-11-10 00:09:18 -0800

    re-formatted comments and generated pretty documentation

--- backend/functions/routers/comments.js
+++ backend/functions/routers/comments.js
@@ -1,9 +1,27 @@
+/** Express router providing comments related routes
+ * @module routers/comments
+ * @requires express
+ * @requires firebase-admin
+ */
+
 var admin = require('firebase-admin');
+
 var express = require('express');
+
+/**
+ * Express router to mount comment related functions on.
+ * @type {object}
+ * @const
+ * @namespace commentsRouter
+ */
 var router = express.Router();
 
 /**
  * Lists all the comments associated with a candidate for a particular event
+ * @name get/list
+ * @function
+ * @memberof module:routers/comments~commentsRouter
+ * @inner
  * @param { string } event_id
  * @param { string } candidate_id
  * @returns { Object[] } a list of all comments for candidateID for the
@@ -15,6 +33,10 @@ router.get('/list', async function (req, res) {
 
 /**
  * Adds a comment to a candidate for a particular event
+ * @name post/add
+ * @function
+ * @memberof module:routers/comments~commentsRouter
+ * @inner
  * @param { string } member_id
  * @param { string } event_id
  * @param { string } candidate_id
@@ -29,6 +51,10 @@ router.post('/add', async (req, res) => {
 
 /**
  * Deletes a comment from comments database
+ * @name delete/list
+ * @function
+ * @memberof module:routers/comments~commentsRouter
+ * @inner
  * @param { string } comment_id
  * @returns { string } a success status message if the comment is deleted
  * successfully, an error message otherwise
