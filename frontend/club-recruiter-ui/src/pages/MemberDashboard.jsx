55a50ec7f2108dfaaf213e2630228c32c99b492d
Cameron Miller <camm73@yahoo.com>
2021-11-10 16:46:01 -0800

    Add join event to dashboard

--- frontend/club-recruiter-ui/src/pages/MemberDashboard.jsx
+++ frontend/club-recruiter-ui/src/pages/MemberDashboard.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import { TextField, Button } from '@mui/material';
+import React, { useState } from 'react';
+import AddIcon from '@mui/icons-material/Add';
+import Header from '../components/Header';
 
-const MemberDashboard = () => (
-  <div>
-    <h1>MANAGEMENT PORTAL DASHBOARD</h1>
-  </div>
-);
+import '../styles/MemberDashboard.css';
+
+const MemberDashboard = () => {
+  const [eventCode, setEventCode] = useState('');
+
+  const handleJoinEvent = () => {
+
+  };
+
+  return (
+    <div className="dashboard">
+      <Header pageName="Member Dashboard" />
+      <div className="join-event">
+        <div className="event-code-wrapper">
+          <TextField className="event-code-field" fullWidth id="outlined-basic" value={eventCode} label="Event Code" variant="outlined" onChange={(e) => setEventCode(e.target.value)} />
+        </div>
+        <Button variant="contained" startIcon={<AddIcon />} disabled={eventCode.length === 0} onClick={handleJoinEvent}>Join Event</Button>
+      </div>
+      <div className="event-list">
+        <h2>Events</h2>
+      </div>
+    </div>
+  );
+};
 
 export default MemberDashboard;
