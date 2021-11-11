10655a53b3261d8a5c3dbc3d2ef426ed6faf7730
tianyu139 <liutianyu100@gmail.com>
2021-11-11 15:30:49 -0800

    Add, promote, demote, delete members/admins from events

--- backend/functions/constants.js
+++ backend/functions/constants.js
@@ -2,5 +2,5 @@
 module.exports.CANDIDATES_COLLECTION = "Candidates";
 module.exports.EVENTS_COLLECTION = "Events";
 module.exports.CLUB_MEMBERS_COLLECTION = "ClubMembers";
-module.exports.MEMBERS_EVENTS_COLLECTION = "ClubMembers_Events";
-module.exports.ADMIN_EVENTS_COLLECTION = "Admins_Events";
+module.exports.EVENT_MEMBERS_COLLECTION = "ClubMembers_Events";
+module.exports.EVENT_ADMINS_COLLECTION = "Admins_Events";
