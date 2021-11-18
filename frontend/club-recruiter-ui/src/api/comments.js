2e79f4d7bf7ddc74027b4e53ade193237a12582e
Cameron Miller <camm73@yahoo.com>
2021-11-18 15:32:41 -0800

    Cleanup

--- frontend/club-recruiter-ui/src/api/comments.js
+++ frontend/club-recruiter-ui/src/api/comments.js
@@ -1,7 +1,7 @@
 const getComment = async (commentID) => {
   console.log(commentID);
   return {
-    commentText: 'This is a test comment. Test comment. Testing. Nlahlskdsf lajdsflkasdf aslkfdjalksjdf dfjslkf sldfksdf lsdkfdskl sdf sldf akl dfkla dsklf kljdsflkasdflkdsf lasf sdf adlkf fl',
+    commentText: 'This is a test comment. Test comment. Testing.',
     memberID: '',
   };
 };
