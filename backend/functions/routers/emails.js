9a9a0be912f0d8046ba8d91173703ae47121aaf5
Andy Cai <andycai20000212@gmail.com>
2021-11-26 16:47:40 -0800

    Email almost done

--- backend/functions/routers/emails.js
+++ backend/functions/routers/emails.js
@@ -3,7 +3,10 @@ const admin = require('firebase-admin');
 const { firestore } = require('firebase-admin');
 const { validateFirebaseIdToken } = require('../auth');
 const nodemailer = require('nodemailer');
-const { CANDIDATES_COLLECTION } = require('../constants');
+const { CANDIDATES_COLLECTION, FROM_EMAIL, EMAIL_TEMPLATE, EVENTS_COLLECTION } = require('../constants');
+const { isAdmin } = require('../util');
+
+require('dotenv').config();
 
 var router = express.Router();
 
@@ -12,50 +15,80 @@ var transporter = nodemailer.createTransport({
   port: 465,
   secure: true,
   auth: {
-    user: '********@gmail.com', // TODO: fill in later
-    pass: '************' // TODO: fill in later
+    user: process.env.EMAIL_ID,
+    pass: process.env.EMAIL_PASS
   }
 });
+
+function formatEmail(email_template, candidate_name, event_name, application_status) {
+  var s = email_template.replace("__{candidate_name}__", candidate_name);
+  s = s.replace("__{event_name}__", event_name);
+  s = s.replace("__{application_status}__", application_status);
+  return s;
+}
 
 /**
  * Sends a basic email to the given candidate
  * @name POST/email
  * @function
  * @param { string } member_id for validation purposes only
- * @param { string } email_content
  * @param { string } email_subject
+ * @param { string } email_body
+ * @param { string } event_id
  * @param { string[] } candidate_ids
  * @returns { Object } member detail with member_id
  *
  */
 router.post('/', validateFirebaseIdToken, async function (req, res) {
-  var { email_content, email_subject, candidate_ids } = req.body;
-  var db = firestore();
+  var member_id = req.user.uid;
+  var { email_subject, email_body, event_id, candidate_ids } = req.body;
 
-  for (var candidate_id in candidate_ids) {
-    var candidateRef = await db
-      .collection(CANDIDATES_COLLECTION)
-      .doc(candidate_id)
-      .get();
-    var target_email = candidateRef.data()['email'];
+  if (!email_body)
+    email_body = EMAIL_TEMPLATE;
 
-    const mail_options = {
-      from: `uclasep@gmail.com`,
-      to: target_email,
-      subject: email_subject,
-      html: `<p>${email_content}</p>`
-    };
-
-    transporter.sendMail(mail_options, (error, data) => {
-      if (error) {
-        console.log(error)
-        res.status(404).send(`Error sending email: ${error}`)
-        return
-      }
-      console.log("Sent!")
-    });
+  if (!(await isAdmin(member_id, event_id))) {
+    res.status(404).send('Non-admins cannot send mass emails!');
+    return;
   }
-  res.status(200);
+
+  try {
+    var db = firestore();
+
+    candidate_ids.forEach(async (candidate_id) => {
+      var candidateRef = await db
+        .collection(CANDIDATES_COLLECTION)
+        .doc(candidate_id)
+        .get();
+      var target_email = candidateRef.data().email;
+
+      var eventRef = await db
+        .collection(EVENTS_COLLECTION)
+        .doc(candidateRef.data().event_id)
+        .get();
+
+      var email_html = formatEmail(email_body, candidateRef.data().name, eventRef.data().name, candidateRef.data().application_status)
+
+      const mail_options = {
+        from: FROM_EMAIL,
+        to: target_email,
+        subject: email_subject,
+        html: email_html
+      };
+
+      transporter.sendMail(mail_options, (error, data) => {
+        if (error) {
+          console.log(error)
+          res.status(404).send(`Error sending email: ${error}`)
+          return
+        }
+        console.log("Sent!")
+      });
+    })
+    res.status(200);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+
 
 });
 
