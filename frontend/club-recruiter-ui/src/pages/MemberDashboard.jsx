5058a06115707fbc2a195b9943ee36fa9c4bc0f0
Cameron Miller <camm73@yahoo.com>
2021-11-26 17:59:39 -0800

    Added sorting of events by organizer status

--- frontend/club-recruiter-ui/src/pages/MemberDashboard.jsx
+++ frontend/club-recruiter-ui/src/pages/MemberDashboard.jsx
@@ -8,7 +8,7 @@ import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import EventCard from '../components/EventCard';
 
-import { listMemberEvents, joinEvent } from '../api/events';
+import { listMemberEvents, joinEvent, isAdmin } from '../api/events';
 
 import '../styles/MemberDashboard.css';
 
@@ -16,12 +16,22 @@ const MemberDashboard = () => {
   const [memberCode, setMemberCode] = useState('');
   const [events, setEvents] = useState([]);
   const [filterChecked, setFilterChecked] = useState(false);
+  const [organizingEvents, setOrganizingEvents] = useState();
   const history = useHistory();
 
   const loadEvents = async () => {
     const memberEvents = await listMemberEvents();
     setEvents(memberEvents);
-    console.log('Loaded list of events for user');
+    const orgEvents = [];
+    if (memberEvents === undefined) return;
+    // eslint-disable-next-line no-restricted-syntax
+    for (const eventID of memberEvents) {
+      const adminStatus = await isAdmin(eventID);
+      if (adminStatus) {
+        orgEvents.push(eventID);
+      }
+    }
+    setOrganizingEvents(orgEvents);
   };
 
   const handleJoinEvent = async () => {
@@ -67,8 +77,8 @@ const MemberDashboard = () => {
           label="Only show events I am organizing"
         />
         <div className="event-list">
-          {
-            events.map(
+          { filterChecked
+            ? organizingEvents.map(
               (eventID) => (
                 <EventCard
                   clickAction={() => {
@@ -81,7 +91,19 @@ const MemberDashboard = () => {
                 />
               ),
             )
-          }
+            : events.map(
+              (eventID) => (
+                <EventCard
+                  clickAction={() => {
+                    history.push(`/event/${eventID}`);
+                  }}
+                  key={eventID}
+                  eventID={eventID}
+                  refreshAction={loadEvents}
+                  hideDescription
+                />
+              ),
+            )}
         </div>
       </div>
     </>
